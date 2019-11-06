f45c9025d41cffd982ecbbdd52844e5a4f074de9
Cong Ma <congm1@andrew.cmu.edu>
2019-11-05 19:43:24 -0500

    update the example commands to run STAR and squid. Since STAR 2.6.0a, outputing a chimeric SAM file is not a default setting.

--- example/STARnSQUIDcommand.sh
+++ example/STARnSQUIDcommand.sh
@@ -2,8 +2,13 @@
 
 
 # STAR indexing and aligning
-if [ $(file sampledata/genome_rearranged.fa.gz) = "gzip" ]; then
+if [[ -e sampledata/genome_rearranged.fa.gz ]] && [[ ! -e sampledata/genome_rearranged.fa ]]; then
 	gunzip sampledata/genome_rearranged.fa.gz
+fi
+
+if [[ ! -e sampledata/genome_rearranged.fa ]]; then
+	echo "No such files: sampledata/genome_rearranged.fa.gz or sampledata/genome_rearranged.fa"
+	exit 1
 fi
 
 mkdir -p sampledata/STARindex
@@ -12,9 +17,9 @@ STAR --runThreadN 4 --runMode genomeGenerate --genomeDir sampledata/STARindex --
 mv Log.out sampledata/STARindex/
 
 mkdir -p sampledata/StarAlign
-STAR --runThreadN 4 --genomeDir sampledata/STARindex/ --readFilesIn sampledata/RNA1.fastq.gz sampledata/RNA2.fastq.gz --outFileNamePrefix sampledata/StarAlign/ --outSAMtype BAM SortedByCoordinate --outReadsUnmapped Fastx --chimSegmentMin 20 --outSAMstrandField intronMotif
-samtools view -Shb sampledata/StarAlign/Chimeric.out.sam sampledata/StarAlign/Chimeric.out.bam
+STAR --runThreadN 4 --genomeDir sampledata/STARindex/ --readFilesIn sampledata/RNA1.fastq.gz sampledata/RNA2.fastq.gz --readFilesCommand zcat --outFileNamePrefix sampledata/StarAlign/ --outSAMtype BAM SortedByCoordinate --outReadsUnmapped Fastx --chimSegmentMin 20 --outSAMstrandField intronMotif --chimOutType SeparateSAMold
+samtools view -Shb sampledata/StarAlign/Chimeric.out.sam -o sampledata/StarAlign/Chimeric.out.bam
 
 
 # SQUID predicting
-./../bin/squid -b sampledata/StarAlign/Aligned.sortedByCoord.out.bam -c sampledataStarAlign/Chimeric.out.bam -G 1 -CO 1 -o squid
+./../bin/squid -b sampledata/StarAlign/Aligned.sortedByCoord.out.bam -c sampledata/StarAlign/Chimeric.out.bam -G 1 -CO 1 -o squid
